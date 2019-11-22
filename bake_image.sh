0a24881a4d4c5390535eb13da0f31f9bc64b0544
Sg <shogo1104@gmail.com>
2019-11-22 18:55:31 +0900

    dockerhub management logic

--- bake_image.sh
+++ bake_image.sh
@@ -2,9 +2,10 @@
 # Constants
 ###################
 BASENAME="tezos_build"
-DOCKERHUB_REPO=$BASENAME"_deps"
+DOCKERHUB_BARE_REPO=$BASENAME"_deps"
+DOCKERHUB_COMPLETED_REPO=tezos
 TAG=latest
-IMAGE_FULLNAME=$DOCKERHUB_REPO:$TAG
+IMAGE_FULLNAME=$DOCKERHUB_BARE_REPO:$TAG
 
 ###################
 # Prepare dockerhub info
@@ -30,19 +31,17 @@ repos=`docker-hub repos --orgname $DOCKERHUB_NAME`
 ###################
 # Check target dockerhub repo is already created or not
 ###################
-if [[ $repos == *$DOCKERHUB_REPO* ]]; then
-  echo "$DOCKERHUB_REPO is already in your dockerhub"
-else
-  echo "Please add $DOCKERHUB_REPO in your dockerhub repos."
-  exit 1
-fi
+hubcount=0
+[[ $repos != *" "$DOCKERHUB_BARE_REPO" "* ]] && hubcount=$(($hubcount+1))
+[[ $repos != *" "$DOCKERHUB_COMPLETED_REPO" "* ]] && hubcount=$(($hubcount+1))
+[ "$hubcount" -gt "0" ] && echo "Please add $DOCKERHUB_BARE_REPO and $DOCKERHUB_COMPLETED_REPO in your dockerhub repos." && exit 1
 
 ###################
 # Explain what will happen
 ###################
 ORG=${2:-cryptoeconomicslab}
 BRANCH=${3:-@sg/michelson-level}
-echo "Dockerhub Target = $DOCKERHUB_NAME/$DOCKERHUB_REPO"
+echo "Dockerhub Target = $DOCKERHUB_NAME/$DOCKERHUB_BARE_REPO"
 echo "Tezos Target = $ORG/tezos#$BRANCH"
 
 ###################
@@ -73,20 +72,25 @@ IMAGES_RAW=`docker images`
 ###################
 # Check target image
 ###################
-if [[ $IMAGES_RAW == *$DOCKERHUB_REPO*$TAG* ]]; then
-  echo "=== Tezos dockerized. ==="
-  cd ./tezos
+if [[ $IMAGES_RAW == *$DOCKERHUB_BARE_REPO*$TAG* ]]; then
+  echo "=== Tezos Bare dockerized. ==="
+  ###################
+  # Push bare image to Dockerhub
+  ###################
+  docker push $DOCKERHUB_NAME/$IMAGE_FULLNAME
 
   ###################
   # Build custom Tezos inside docker
   ###################
+  cd ./tezos
   scripts/ci/create_docker_image.build.sh $DOCKERHUB_NAME/$BASENAME
+  cd ../
 
   ###################
-  # Push it to Dockerhub
+  # Push completed image to Dockerhub
   ###################
-  docker push $IMAGE_FULLNAME
-  cd ../
+  docker push $DOCKERHUB_NAME/$DOCKERHUB_COMPLETED_REPO:latest
+  echo "=== Tezos Bare dockerized. ==="
 else
  echo "Tezos dockerization failed."
 fi

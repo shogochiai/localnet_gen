01762b8457361f69b3b27debaabdd14ae8c9ee84
Sg <shogo1104@gmail.com>
2019-11-22 16:15:01 +0900

    Add default args

--- bake_image.sh
+++ bake_image.sh
@@ -1,5 +1,13 @@
-DOCKERHUB_NAME=sgtn
-BRANCH=@sg/michelson-level
+BASENAME="tezos_build"
+DOCKERHUB_REPO=$BASENAME"_deps"
+TAG=latest
+IMAGE_FULLNAME=$DOCKERHUB_REPO:$TAG
+
+DOCKERHUB_NAME=${1:-sgtn}
+BRANCH=${2:-@sg/michelson-level}
+echo "Dockerhub Target = $DOCKERHUB_NAME/$DOCKERHUB_REPO"
+echo "Tezos Branch = $BRANCH"
+
 
 [ ! -d "./tezos" ] && git clone git@gitlab.com:tezos/tezos.git
 if [ -d "./tezos" ]; then
@@ -8,10 +16,6 @@ if [ -d "./tezos" ]; then
   cd ../
 fi
 
-BASENAME="tezos_build"
-DOCKERHUB_REPO=$BASENAME"_deps"
-TAG=latest
-IMAGE_FULLNAME=$DOCKERHUB_REPO:$TAG
 docker image build -t $IMAGE_FULLNAME --ulimit nofile=1024 .
 IMAGES_RAW=`docker images`
 

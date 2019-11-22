387ab1615b2e39f65d7563f97d0506cf28677d8e
Sg <shogo1104@gmail.com>
2019-11-22 15:35:57 +0900

    Refactoring

--- bake_image.sh
+++ bake_image.sh
@@ -1,12 +1,25 @@
+DOCKERHUB_NAME=sgtn
+BRANCH=@sg/michelson-level
+
 [ ! -d "./tezos" ] && git clone git@gitlab.com:tezos/tezos.git
-DC_REPO_SHORT="tezos_build"
-DC_TAG=$DC_REPO_SHORT"_deps:latest"
-docker image build -t $DC_TAG --ulimit nofile=1024 .
-DC_RES=`docker images`
-if [[ $DC_RES == *$DC_TAG* ]]; then
+if [ -d "./tezos" ]; then
+  cd tezos
+  git checkout $BRANCH
+  cd ../
+fi
+
+BASENAME="tezos_build"
+DOCKERHUB_REPO=$BASENAME"_deps"
+TAG=latest
+IMAGE_FULLNAME=$DOCKERHUB_REPO:$TAG
+docker image build -t $IMAGE_FULLNAME --ulimit nofile=1024 .
+IMAGES_RAW=`docker images`
+
+if [[ $IMAGES_RAW == *$DOCKERHUB_REPO*$TAG* ]]; then
   echo "=== Tezos dockerized. ==="
   cd ./tezos
-  scripts/ci/create_docker_image.build.sh sgtn/$DC_REPO_SHORT
+  scripts/ci/create_docker_image.build.sh $DOCKERHUB_NAME/$BASENAME
+  docker push $IMAGE_FULLNAME
   cd ../
 else
  echo "Tezos dockerization failed."
